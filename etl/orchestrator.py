7ec7359ffd7d7ca8abcfa4a0b8907b6f4c36e5f7
rgb-420 <73raul7373@gmail.com>
2026-03-03 17:09:00 +0100

    Remove deprecated master and boats DB pipelines

--- etl/orchestrator.py
+++ etl/orchestrator.py
@@ -1,6 +1,5 @@
 from etl.pipelines.scrape_pipelines import run_scrape_pipeline
-from etl.pipelines.master_boats_pipeline import run_master_boats_pipeline
-from etl.pipelines.regattas_pipeline import run_regattas_pipeline
+from etl.pipelines.boats_pipeline import run_boats_pipeline
 from etl.pipelines.classes_pipeline import run_classes_pipeline
 from etl.pipelines.clubs_pipeline import run_clubs_pipeline
 
@@ -8,7 +7,6 @@ def run_full_pipeline():
     print("Starting full pipeline...")
 
     #run_scrape_pipeline()
-    #run_master_boats_pipeline()
     #run_regattas_pipeline()
     #run_classes_pipeline()
     run_clubs_pipeline()
