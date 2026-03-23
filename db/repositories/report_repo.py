476b7a37332ccbd5f28d2e20808193422029d906
rgb-420 <73raul7373@gmail.com>
2026-03-23 19:56:36 +0100

    Inserted first version of monday report

---
+++ db/repositories/report_repo.py
@@ -0,0 +1,41 @@
+from sqlalchemy import text, bindparam
+
+def get_monday_report(conn, clubs, regattas):
+    query = text("""
+        SELECT DISTINCT
+            r.name AS regatta,
+            o.name AS owner,
+            b.name AS boat,
+            rl.url AS results_link
+
+        FROM yacht_db.boats b
+
+        JOIN yacht_db.boats_owner bo 
+            ON bo.id_boat = b.id_boat
+        JOIN yacht_db.owners o 
+            ON o.id_owner = bo.id_owner
+
+        JOIN yacht_db.boat_clubs bc 
+            ON bc.id_boat = b.id_boat
+        JOIN yacht_db.clubs c 
+            ON c.id_club = bc.id_club
+
+        JOIN yacht_db.boat_editions be 
+            ON be.id_boat = b.id_boat
+        JOIN yacht_db.regatta_editions re 
+            ON re.id_edition = be.id_edition
+        JOIN yacht_db.regattas r 
+            ON r.id_regatta = re.id_regatta
+
+        LEFT JOIN yacht_db.regatta_links rl
+            ON re.id_edition = rl.id_edition
+
+        WHERE c.name IN :clubs
+        AND r.name IN :regattas
+    """)
+
+    query = query.bindparams(bindparam("clubs", expanding=True), bindparam("regattas", expanding=True))
+
+    result = conn.execute(query, {"clubs": clubs, "regattas": regattas}).fetchall()
+
+    return result 
---
+++ generate_monday_report.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+from db.connection import get_engine
+from db.repositories.report_repo import get_monday_report
+
+
+def main():
+    engine = get_engine()
+
+    clubs = ["RTYC", "ROYAL THAMES YC", "ROYAL THAMES YACHT CLUB"]
+    regattas = [
+        "British Classic Week",
+        "Cowes Week"
+    ]
+
+    with engine.connect() as conn:
+        rows = get_monday_report(conn, clubs, regattas)
+
+    columns = ["regatta", "owner", "boat", "results_link"]
+    df = pd.DataFrame(rows, columns=columns)
+
+    df.to_csv("data/report/monday_report.csv", index=False)
+
+    print("Monday report generado correctamente")
+
+
+if __name__ == "__main__":
+    main()
