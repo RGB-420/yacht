d2fee6737a1cae09826180e4ed262b57af61c05a
rgb-420 <73raul7373@gmail.com>
2026-05-05 19:42:13 +0200

    Added mode ssl to database connection

--- db/connection.py
+++ db/connection.py
@@ -22,6 +22,7 @@ def get_engine():
             host=host,
             port=port,
             database=database,
+            connect_args={"sslmode": "require"}
         )
 
     engine = create_engine(url, echo=False, future=True, pool_pre_ping=True)
